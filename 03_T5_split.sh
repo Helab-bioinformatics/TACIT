808e237d22804596f2da91041cdb80837f2f20f0
Helab-bioinformatics <51817330+Helab-bioinformatics@users.noreply.github.com>
2023-07-20 16:16:37 +0800

    Delete 03_T5_split.sh

--- 03_T5_split.sh
+++
@@ -1,4 +0,0 @@
-for i in `ls *.1.fq.gz`; do base=$(basename $i ".1.fq.gz"); mkdir ${base}.R1/; perl ../splitbarcode0bp.pl ${base}.1.fq.gz 0 AAAGAA CAAGTC GAAGGG ${base}.R1/T5_${base}_1.1.fq.gz ${base}.R1/T5_${base}_2.1.fq.gz ${base}.R1/T5_${base}_3.1.fq.gz ${base}.R1/T5_${base}_noid.1.fq.gz; done
-
-
-
