de4c7215bfab3b544fe25802b528323404d21a36
Helab-bioinformatics <51817330+Helab-bioinformatics@users.noreply.github.com>
2024-10-23 23:09:40 +0800

    Delete TACIT directory

--- TACIT/fig4/05_homer.sh
+++
@@ -1,3 +0,0 @@
-findMotifsGenome.pl  top1920_kmeans_toti_active.bed  mm10  top1920_kmeans_toti_active/   -mask   -size given   -len 6,8,10,12 &
-
-findMotifsGenome.pl  top1920_kmeans_toti_active_3x.bed  mm10  top1920_kmeans_toti_active_3x/   -mask   -size given   -len 6,8,10,12 &
--- TACIT/scRNA-seq/00_split_single_cells/01_02_run.sh
+++
@@ -1,2 +0,0 @@
-sh 01_split96sc.R2.sh
-sh 02_pick.sh
